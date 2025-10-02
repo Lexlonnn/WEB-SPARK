3a978fc429b71804ac3bb51f4816e7d9fca88143
Lexlonnn <ravishrb8@gmail.com>
2025-10-02 21:46:27 +0530

    meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,8 @@ import { Suspense } from "react"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Web Spark",
+  description: "ACM",
 }
 
 export default function RootLayout({
